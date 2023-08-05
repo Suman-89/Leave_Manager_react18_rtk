7b8dc8720c280660e3c02b46a0e405396ea34d6d
Suman-89 <suman.mukherjee01@gmail.com>
2023-08-05 19:38:47 +0530

    status , view done

--- src/redux/action/myDetailsAction.js
+++ src/redux/action/myDetailsAction.js
@@ -4,8 +4,9 @@ import API from '../../api'
 
 export const MyDetailsAction = createAsyncThunk('empLeave/detail', async ()=>{
     const leaveDetails = await API.get(`/wp-jwt/v1/apply-leave-details`)
-    console.log('leaveDetails:',leaveDetails.data.data)
+    // console.log('leaveDetails:',leaveDetails.data.data.reverse())
     if(leaveDetails.status === 200){
         return leaveDetails.data.data
+        // return leaveDetails.data.data
     }
 })
--- src/views/pages/leave-application/MyDetails.js
+++ src/views/pages/leave-application/MyDetails.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { CButton, CCard, CCardBody, CCardHeader, CCol, CFormInput, CFormSwitch, CRow, CSpinner } from '@coreui/react'
+import { CButton, CCard, CCardBody, CCardHeader, CCol, CForm, CFormInput, CInputGroup, CInputGroupText, CModal, CModalBody, CModalHeader, CModalTitle, CRow, CSpinner } from '@coreui/react'
 import React, { useEffect, useState } from 'react'
 import DataTable from 'react-data-table-component'
 import { useDispatch, useSelector } from 'react-redux'
@@ -15,15 +15,19 @@ const MyDetails = () => {
   const [filteredUserInfo, setFilteredUserInfo] = useState([])
   const [searchItem, setSearchItem] = useState("")
   //switch
-  const [checked,setChecked] = useState(false)
+  const [checked, setChecked] = useState(false)
+  //view modal //
+  const [openModal, setOpenModal] = useState(false)
+  const [viewDetails,setViewDetails] = useState({})
 
-  console.log('userInfo:',userInfo)
+  console.log('userInfo:', userInfo)
+
 
   const handleSearch = async () => {
     try {
       const sData = await API.get(`/wp-jwt/v1/apply-leave-details`)
       console.log('sData:', sData.data.data)
-      setFilteredUserInfo(sData.data.data)
+      setFilteredUserInfo(sData.data.data.reverse())
     } catch (err) {
       console.log('error:', err)
     }
@@ -35,75 +39,133 @@ const MyDetails = () => {
   }, [])
 
   useEffect(() => {
-    const searchResult = userInfo.filter((userResult) =>{
+    const searchResult = userInfo.filter((userResult) => {
       return userResult.display_name.toLowerCase().match(searchItem.toLowerCase())
     })
     setFilteredUserInfo(searchResult)
   }, [searchItem])
 
-  const viewClick = (vId)=>{
-    console.log('vId:',vId)
+  const viewClick = (data) => {
+    console.log('vId data:', data)
+    setViewDetails(data)
+    setOpenModal(true)
   }
 
-  const StartDate =({row}) => moment().format('Do-MMM-yyyy')
-  const EndDate =({row}) => moment().format('Do-MMM-yyyy')
+  const StartDate = ({ row }) => moment(row).format('DD-MMM-YYYY')
+  const EndDate = ({ row }) => moment(row).format('DD-MMM-YYYY')
 
 
   const columns = [
     {
       name: 'Sl. No.',
-      selector: (row, index) => index + 1
+      selector: (row, index) => <p>{index + 1}.</p>
     },
     {
       name: 'Project Manager',
       selector: (row) => row.display_name,
-      sortable :true
+      sortable: true
     },
     {
       name: 'Department',
       selector: (row) => row.dept_name,
-      sortable :true
+      sortable: true
     },
     {
       name: 'Start Date',
-      selector: (row) => row.start_date,
-      cell:(sdRow) => <StartDate row={sdRow} />,
-      sortable :true
+      // selector: (row) => row.start_date,
+      cell: (sdRow) => <StartDate row={sdRow.start_date} />,
+      sortable: true
     },
     {
       name: 'End Date',
-      selector: (row) => row.end_date,
-      cell:(edRow) => <EndDate row={edRow} />,
-      sortable :true
+      // selector: (row) => row.end_date,
+      cell: (edRow) => <EndDate row={edRow.end_date} />,
+      sortable: true
     },
     {
       name: 'Status',
-      cell: (row) => row.status,
+      cell: (row) =>
+        <CButton
+          color={
+            row.status === "0" ? 'warning'
+              : row.status === "1" ? 'success'
+                : row.status === "2" ? 'danger'
+                  : row.status === "3" ? 'primary'
+                    : row.status === "4" ? 'info'
+                      : row.status === "5" ? 'secondary' : ''}
+
+          shape="rounded-0"
+          size='sm'
+          readOnly
+          style={{ width: '100%', cursor: 'text' }}
+        >
+          {row.status === "0" ? 'Pending'
+            : row.status === "1" ? 'Approved'
+              : row.status === "2" ? 'Rejected'
+                : row.status === "3" ? 'On Hold'
+                  : row.status === "4" ? 'Modified'
+                    : row.status === "5" ? 'Cancelled' : ''}
+        </CButton>,
     },
     {
       name: 'View/Chat',
-      cell: (row) => <div className='col' style={{ alignItems: 'center' }}>
-        <CButton className='rounded-pill' 
-        onClick={()=>viewClick(row.id)}
-      >
-        <i className='fa fa-eye'></i>
-      </CButton>&nbsp;
+      cell: (row) => <div className='col' style={{ margin: '0 auto' }}>
+        <CButton className='rounded-pill' onClick={() => viewClick(row)}>
+          <i className='fa fa-eye'></i></CButton>&nbsp;
+        
         <CButton color='success' className='rounded-pill'
         >
-          <i className='far fa-comment-dots'></i>
+          <i className="far fa-comment-dots fa-md" style={{ color: '#fff' }}></i>
         </CButton></div>
     },
     {
       name: 'Action',
       cell: (row) => <ReactSwitch
-      onChange={() => setChecked(true)}
-      checked={checked}
-      className="react-switch"
-    />
+        onChange={() => setChecked(true)}
+        checked={checked}
+        className="react-switch"
+      />
     },
   ]
   return (
     <>
+     <CModal alignment="center" size="sm" visible={openModal} onClose={() => setOpenModal(false)}>
+          <CModalHeader>
+            <CModalTitle>Leave Details</CModalTitle>
+          </CModalHeader>
+          <CModalBody>
+            <span>status : <h2 className='display-7'>
+            {viewDetails.status === "0" ? 'Pending'
+            : viewDetails.status === "1" ? 'Approved'
+              : viewDetails.status === "2" ? 'Rejected'
+                : viewDetails.status === "3" ? 'On Hold'
+                  : viewDetails.status === "4" ? 'Modified'
+                    : viewDetails.status === "5" ? 'Cancelled' : ''}
+              </h2></span>
+            <CInputGroup size="sm" className="mb-3">
+              <CForm className="row g-3">
+                <CCol md={6}>
+                  <CFormInput type="text" label="Start Date" value={viewDetails.start_date} readOnly />
+                </CCol>
+                <CCol md={6}>
+                  <CFormInput type="text" label="End Date" value={viewDetails.end_date} readOnly />
+                </CCol>
+              </CForm>
+            </CInputGroup>
+            <CInputGroup size="sm" className="mb-3">
+              <CInputGroupText id="basic-addon1">Department</CInputGroupText>
+              <CFormInput type="text" value={viewDetails.dept_name} readOnly />
+            </CInputGroup>
+            <CInputGroup size="sm" className="mb-3">
+              <CInputGroupText id="basic-addon1">Reason</CInputGroupText>
+              <CFormInput type="text" value={viewDetails.reason} readOnly />
+            </CInputGroup>
+            <CInputGroup size="sm" className="mb-3">
+              <CInputGroupText id="basic-addon1">Supervisor</CInputGroupText>
+              <CFormInput type="text" value={viewDetails.display_name} readOnly />
+            </CInputGroup>
+          </CModalBody>
+        </CModal> 
       <CRow>
         <CCol xs={12}>
           <CCard className="mb-4">
@@ -113,29 +175,29 @@ const MyDetails = () => {
             <CCardBody>
               {
                 isLoading === true ? (
-                  <CSpinner color="secondary"/>
-                ):(
+                  <CSpinner color="secondary" />
+                ) : (
                   <DataTable
-                  columns={columns}
-                  data={filteredUserInfo}
-                  pagination
-                  paginationPerPage={5}
-                  paginationRowsPerPageOptions={[5, 10, 15, 20, 25, 30]}
-                  highlightOnHover
-                  subHeader
-                  subHeaderComponent={
-                    <>
-                      <CFormInput className='w-50 form-control' type="text"
-                        placeholder="Search by name/ department"
-                        value={searchItem}
-                        onChange={(e)=>setSearchItem(e.target.value)}
-                         />
-                      <CButton className='mx-2 col-2' color="primary" onClick={()=>setSearchItem('')} >Reset</CButton>
-                    </>
-                  }
-                />
+                    columns={columns}
+                    data={filteredUserInfo}
+                    pagination
+                    paginationPerPage={5}
+                    paginationRowsPerPageOptions={[5, 10, 15, 20, 25, 30]}
+                    highlightOnHover
+                    subHeader
+                    subHeaderComponent={
+                      <>
+                        <CFormInput className='w-50 form-control' type="text"
+                          placeholder="Search by name/ department"
+                          value={searchItem}
+                          onChange={(e) => setSearchItem(e.target.value)}
+                        />
+                        <CButton className='mx-2 col-2' color="primary" onClick={() => setSearchItem('')} >Reset</CButton>
+                      </>
+                    }
+                  />
                 )
-              }     
+              }
             </CCardBody>
           </CCard>
         </CCol>
